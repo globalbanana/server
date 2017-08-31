7341bef2ab885455d8cd8198a8d89e8d58547968
waheng <peterchang@yahoo-inc.com>
2017-08-31 10:48:54 +0800

    Updated: time of Cron postVideo

--- cronPostVideo.js
+++ cronPostVideo.js
@@ -3,7 +3,7 @@ require('newrelic');
 
 console.log('Cron job is ready for Every 2 hour:')
 
-cron.schedule('0 */2 * * *', function(){
+cron.schedule('0 1,8,10,12,14,16,18,20,22 * * *', function(){
     var shell = require('./module/childProcess/child_helper');
     var commandList = ["npm run postVideo"]
     shell.series(commandList , function(err){
--- postVideo.js
+++ postVideo.js
@@ -1,16 +1,17 @@
 
 import {getAccessToken, videoPost} from './module/facebook';
 import {initDB, videoList, videoUpdate} from './module/dataBase';
+import UserModel from './module/dataBase/user'
 
 (async function () {
   initDB()
   await getAccessToken()
 
+  const getNewestToken = async () => (await UserModel.getList({limit:1, sort: '-createdAt'}))[0].longToken
 
-  const getPublishVideo =() => videoList({}, {status: 'READY'})
+  const getOneReadyVideo = async () => (await videoList({}, {status: 'READY'}))[0]
 
-  const postVideoToFacebook = (_video) => {
-    const accessToken = process.env.FB_ACCESS_TOKEN
+  const postVideoToFacebook = (_video, longtoken) => {
     const pageId = process.env.FB_PUBLISH_PAGE_ID
     const {title, newTitle, description, newDescription, originThumb, s3Source, editedSource} = _video
 
@@ -21,7 +22,7 @@ import {initDB, videoList, videoUpdate} from './module/dataBase';
     }
 
     const videoUrl = editedSource || s3Source
-    return videoPost (accessToken, pageId, videoUrl, payload) 
+    return videoPost (longtoken, pageId, videoUrl, payload) 
   }
 
   const updatePublishedDB = (videoId, publishedVideoId) => {
@@ -30,15 +31,21 @@ import {initDB, videoList, videoUpdate} from './module/dataBase';
       status: 'PUBLISHED', 
       publishedFbId: publishedVideoId
     }
-    
     return videoUpdate(condition, payload)
   }
 
-  const video = (await getPublishVideo())[0]
+  const newestToken = await getNewestToken()
+  
+  const video = await getOneReadyVideo()
+  
+  console.log(`Going to publish '${video.fbPageName}' ...`)
 
-  const publishedVideoId = (await postVideoToFacebook(video)).id
+  const publishedVideoId = (await postVideoToFacebook(video, newestToken)).id
+
+  console.log(`     Video is posted on FB: ${publishedVideoId}`)
 
   await updatePublishedDB(video._id, publishedVideoId)
 
+  console.log(`     DB updated ... `)
   process.exit()
 }())
